596f67c7aab9cda334bcf06000bab1af56965dd6
frushich@gmail.com <frushich@gmail.com>
2015-10-11 13:07:25 -0400

    plot 1 files

---
+++ plot1.R
@@ -0,0 +1,17 @@
+# Set working diretory
+setwd("C:/Users/postdoc/Desktop/learn_R/")
+# Export data
+data <- read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
+# Set Date column as class Date
+data$Date <- as.Date(data$Date,"%d/%m/%Y")
+# Subset Data only to Feb 1 and Feb 2 2007
+data_cut <- subset(data,Date=="2007-02-01" | Date=="2007-02-02")
+# Set Time column as class POSIXlt 
+data_cut$time_tmp <- paste(data_cut$Date, data_cut$Time)
+df <- as.data.frame(data_cut)
+df$Time <- strptime(df$time_tmp, format = "%Y-%m-%d %H:%M:%S")
+
+# Plot Graph # 1
+png(filename="plot1.png",width=480,height=480)
+hist(df$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
